11e1d90cdc98774e54e40ae69084c8ad144a9093
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-07-10 22:06:24 +0200

    esLint update

--- scripts/main_index/index.js
+++ scripts/main_index/index.js
@@ -1,21 +1,23 @@
-async function getDataJson () {                                  
-    const response = await fetch ("data/recipes.json");
-    const recipes = await response.json();
-    return recipes;
+async function getDataJson() {
+  const response = await fetch('data/recipes.json');
+  const recipes = await response.json();
+  return recipes;
 }
 
 async function displayData(recipes) {
-    const recipeSection = document.querySelector("#recipes_cards");
-
-    recipes.forEach((recipe) => {
-        const recipeCard = getRecipeCard(recipe);
-        recipeSection.appendChild(recipeCard);
-    });
-};
+  const recipeSection = document.querySelector('#recipes_cards');
+  recipeSection.innerHTML = '';
+  recipes.forEach((recipe) => {
+    /** getRecipeCard is defined in recipes_cards */
+    // eslint-disable-next-line no-undef
+    const recipeCard = getRecipeCard(recipe);
+    recipeSection.appendChild(recipeCard);
+  });
+}
 
 async function init() {
-    const { recipes } = await getDataJson();
-    displayData(recipes);
-};
+  const { recipes } = await getDataJson();
+  displayData(recipes);
+}
 
 init();
