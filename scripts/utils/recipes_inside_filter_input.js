42df41d238342d7ac4ed7168df7c1a6e6e9de696
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-07-10 22:38:36 +0200

    Update recipes_inside_filter_input.js

--- scripts/utils/recipes_inside_filter_input.js
+++ scripts/utils/recipes_inside_filter_input.js
@@ -1,86 +1,96 @@
+/* eslint-disable no-plusplus */
 async function getDataRecipes() {
-    const response = await fetch ("data/recipes.json");
-    return await response.json();
+  const response = await fetch('data/recipes.json');
+  // eslint-disable-next-line no-return-await
+  return await response.json();
 }
-
-/* *Filter selection input */ 
-/***************GET A SPECIFIC INGREDIENT VIA INPUT********************/
-async function getDetailedFilterIngredients(){
-    getDataRecipes();
-    const filterRender = document.querySelectorAll('.list_ingredients span');
-    let cards = document.querySelectorAll('span.filter-item-ingredient');
-    let search_query = document.getElementById("ingredient-input").value;
-    console.log(search_query);
-    console.log(cards[0].innerText);
-    for (var i = 0; i < cards.length; i++) {
-      if(cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
-            cards[i].classList.remove("is-hidden");
-      } else {
-        cards[i].classList.add("is-hidden");
-      }
-    }
-    return(filterRender);
-}
-
-async function getInputIngredient(){
-    let searchInputIngredient = document.getElementById('ingredient-input');
-    searchInputIngredient.addEventListener('keyup', () => {
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(getDetailedFilterIngredients, typeInterval);
-    });
-}
-
-/***************GET A SPECIFIC APPLIANCE VIA INPUT********************/
-
-async function getDetailedFilterAppliance(){
+/* *Filter selection input */
+/* **************GET A SPECIFIC INGREDIENT VIA INPUT******************* */
+async function getDetailedFilterIngredients() {
   getDataRecipes();
-  const filterRender = document.querySelectorAll('.list_appliance span');
-  let cards = document.querySelectorAll('span.filter-item-appliance');
-  let search_query = document.getElementById("appliance-input").value;
-  console.log(search_query);
-  console.log(cards[0].innerText);
-  for (var i = 0; i < cards.length; i++) {
-    if(cards[i].innerText.toLowerCase()
-      .includes(search_query.toLowerCase())) {
-        cards[i].classList.remove("is-hidden");
+  const filterRender = document.querySelectorAll('.list_ingredients span');
+  const cards = document.querySelectorAll('span.filter-item-ingredient');
+  const searchQuery = document.getElementById('ingredient-input').value;
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].innerText.toLowerCase().includes(searchQuery.toLowerCase())) {
+      cards[i].classList.remove('is-hidden');
     } else {
-      cards[i].classList.add("is-hidden");
+      cards[i].classList.add('is-hidden');
     }
   }
-  return(filterRender);
+  return (filterRender);
 }
-
-async function getInputAppliance(){
-  let searchInputAppliance = document.getElementById('appliance-input');
-  searchInputAppliance.addEventListener('keyup', () => {
-      clearTimeout(typingTimer);
-      typingTimer = setTimeout(getDetailedFilterAppliance, typeInterval);
+/** getInputIngredient is defined in tags_generator */
+// eslint-disable-next-line no-unused-vars
+async function getInputIngredient() {
+  const searchInputIngredient = document.getElementById('ingredient-input');
+  searchInputIngredient.addEventListener('keyup', () => {
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    clearTimeout(typingTimer);
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    typingTimer = setTimeout(getDetailedFilterIngredients, typeInterval);
   });
 }
 
-/***************GET A SPECIFIC USTENCIL VIA INPUT********************/
-async function getDetailedFilterUstencil(){
+/* **************GET A SPECIFIC APPLIANCE VIA INPUT******************* */
+
+async function getDetailedFilterAppliance() {
   getDataRecipes();
-  const filterRender = document.querySelectorAll('.list_ustencils span');
-  let cards = document.querySelectorAll('span.filter-item-ustencils');
-  let search_query = document.getElementById("ustencils-input").value;
-  console.log(search_query);
-  console.log(cards[0].innerText);
-  for (var i = 0; i < cards.length; i++) {
-    if(cards[i].innerText.toLowerCase()
-      .includes(search_query.toLowerCase())) {
-        cards[i].classList.remove("is-hidden");
+  const filterRender = document.querySelectorAll('.list_appliance span');
+  const cards = document.querySelectorAll('span.filter-item-appliance');
+  const searchQuery = document.getElementById('appliance-input').value;
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].innerText.toLowerCase()
+      .includes(searchQuery.toLowerCase())) {
+      cards[i].classList.remove('is-hidden');
     } else {
-      cards[i].classList.add("is-hidden");
+      cards[i].classList.add('is-hidden');
     }
   }
-  return(filterRender);
+  return (filterRender);
+}
+/** getInputAppliance is defined in tags_generator */
+// eslint-disable-next-line no-unused-vars
+async function getInputAppliance() {
+  const searchInputAppliance = document.getElementById('appliance-input');
+  searchInputAppliance.addEventListener('keyup', () => {
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    clearTimeout(typingTimer);
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    typingTimer = setTimeout(getDetailedFilterAppliance, typeInterval);
+  });
 }
 
-async function getInputUstencil(){
-  let searchInputUstencil = document.getElementById('ustencils-input');
+/* **************GET A SPECIFIC USTENCIL VIA INPUT******************* */
+async function getDetailedFilterUstencil() {
+  getDataRecipes();
+  const filterRender = document.querySelectorAll('.list_ustencils span');
+  const cards = document.querySelectorAll('span.filter-item-ustencils');
+  const searchQuery = document.getElementById('ustencils-input').value;
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].innerText.toLowerCase()
+      .includes(searchQuery.toLowerCase())) {
+      cards[i].classList.remove('is-hidden');
+    } else {
+      cards[i].classList.add('is-hidden');
+    }
+  }
+  return (filterRender);
+}
+/** getInputUstencil is defined in tags_generator */
+// eslint-disable-next-line no-unused-vars
+async function getInputUstencil() {
+  const searchInputUstencil = document.getElementById('ustencils-input');
   searchInputUstencil.addEventListener('keyup', () => {
-      clearTimeout(typingTimer);
-      typingTimer = setTimeout(getDetailedFilterUstencil, typeInterval);
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    clearTimeout(typingTimer);
+    /** typingTimer is defined in recipes_input_selector */
+    // eslint-disable-next-line no-undef
+    typingTimer = setTimeout(getDetailedFilterUstencil, typeInterval);
   });
 }
